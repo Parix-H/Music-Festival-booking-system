f538001da143fef7d98e12b4eb69716e2be6530d
Parix <parix.harix@gmail.com>
2019-10-09 10:51:06 +1000

    first commit

---
+++ administrator/components/com_akeeba/BackupEngine/serverkey.php
@@ -0,0 +1,1 @@
+<?php defined('AKEEBAENGINE') or die(); define('AKEEBA_SERVERKEY', 'aMBmDclKqCMQaRkk+0B8BW27UmyrWtQH5FPXZV4tqASpr3VQ6jcPwKmtduhYtHjgJ26CFhwkm6DKx6lFjLFOZA=='); ?>
---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-12-02 10:51:35 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-12-02T10:51:35+00:00	INFO 58.96.56.199	joomlafailure	Username and password do not match or you do not have an account yet.
